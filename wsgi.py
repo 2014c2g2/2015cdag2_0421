c6742db2900df6ab83cae24a27cdae0502d90446
40223104 <40223104@gm.nfu.edu.tw>
2015-04-21 18:43:26 +0800

    add gear4 meshing unfinished

--- wsgi.py
+++ wsgi.py
@@ -913,11 +913,15 @@ class Hello(object):
     n_g2 = '''+str(N)+'''/2
     # 第3齒輪齒數
     n_g3 ='''+str(N)+'''
+    # 第4齒輪齒數
+    n_g4 ='''+str(N)+'''/2
+
 
     # 計算兩齒輪的節圓半徑
     rp_g1 = m*n_g1/2
     rp_g2 = m*n_g2/2
     rp_g3 = m*n_g3/2
+    rp_g4 = m*n_g4/2
 
     # 繪圖第1齒輪的圓心座標
     x_g1 = 400
@@ -928,6 +932,11 @@ class Hello(object):
     # 第3齒輪的圓心座標
     x_g3 = x_g1 + rp_g1 + 2*rp_g2 + rp_g3
     y_g3 = y_g1
+
+    # 第4齒輪的圓心座標
+    x_g4 = x_g1 + rp_g1 + 2*rp_g2 + 2* rp_g3 + rp_g4
+    y_g4 = y_g1
+
 
     # 將第1齒輪順時鐘轉 90 度
     # 使用 ctx.save() 與 ctx.restore() 以確保各齒輪以相對座標進行旋轉繪圖
@@ -972,8 +981,19 @@ class Hello(object):
 
     # 按照上面三個正齒輪的囓合轉角運算, 隨後的傳動齒輪轉角便可依此類推, 完成6個齒輪的囓合繪圖
 
+    #第4齒輪
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g4, y_g4)
+    # rotate to engage
+    ctx.rotate(pi/2-2*pi/n_g4+(pi+pi/n_g3)*n_g3/n_g4)
+    # put it back
+    ctx.translate(-x_g4, -y_g4)
+    spur.Spur(ctx).Gear(x_g4, y_g4, rp_g4, n_g4, pa, "green")
+    ctx.restore()
+
     </script>
-    <canvas id="plotarea" width="1200" height="1200"></canvas>
+    <canvas id="plotarea" width="1400" height="1400"></canvas>
     </body>
     </html>
     '''
